6d974376026897f79759eb0965361d8cb4abe6fc
missmarpleholmes <97935893+aishah0311@users.noreply.github.com>
2025-08-04 14:18:30 +0800

    Add files via upload

---
+++ Restaurant+Orders+MySQL/sql 2.sql
@@ -0,0 +1,12 @@
+-- 1. View the order_details table.
+SELECT * FROM order_details;
+
+-- 2. What is the date range of the table?
+
+-- 3. How many orders were made within this date range?
+
+-- 4. How many items were ordered within this date range?
+
+-- 5. Which orders had the most number of items?
+
+-- 6. How many orders had more than 12 items?
